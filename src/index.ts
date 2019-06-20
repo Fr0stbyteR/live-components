262cfd525c18d3c5872e06a98c117eb40cbb8495
Fr0stbyteR <renshihong@hotmail.com>
2019-06-20 23:14:29 +0200

    clean up

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,4 @@ import LiveNumbox from "./Numbox";
 import LiveTab from "./Tab";
 import LiveMeter from "./Meter";
 
-export { LiveButton, LiveSlider, LiveDial, LiveToggle, LiveText, LiveNumbox, LiveTab, LiveMeter }
+export { LiveButton, LiveSlider, LiveDial, LiveToggle, LiveText, LiveNumbox, LiveTab, LiveMeter };
